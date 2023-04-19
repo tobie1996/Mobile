a46d469581797a53f65b3fd393cdffd1e95646f8
tobi <tobiemba@gmail.com>
2023-04-19 13:45:58 +0100

    co

--- lib/utils/my_firebase.dart
+++ lib/utils/my_firebase.dart
@@ -2,7 +2,7 @@ import 'package:cloud_firestore/cloud_firestore.dart';
 
 abstract class MyFirebase {
   static final _db = FirebaseFirestore.instance;
-  static final contactsCollection = _db.collection('contacts');
+  static final contactsCollection = _db.collection('employe');
 }
 
 abstract class MyFirebase2 {
